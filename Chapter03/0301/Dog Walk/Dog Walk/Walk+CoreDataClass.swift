03fa1ee878e59eadedaa0ef0ce4d91181a273600
CoderDream <coderdream@gmail.com>
2019-07-04 17:41:36 +0800

    0301 A walk down persistence lane  20190704

---
+++ Chapter03/0301/Dog Walk/Dog Walk/Walk+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Walk+CoreDataClass.swift
+//  Dog Walk
+//
+//  Created by CoderDream on 2019/7/4.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Walk)
+public class Walk: NSManagedObject {
+
+}
